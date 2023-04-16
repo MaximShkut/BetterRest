0abb2016c33b11fc5384b655cc0b29190d5edfe4
MaximShkut <m.shkut344@gmail.com>
2023-04-16 07:43:54 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by user236450 on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
