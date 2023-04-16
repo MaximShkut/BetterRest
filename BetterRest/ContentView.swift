0ce52a3e7aa786f9cf08903706cd504c88dd39e1
MaximShkut <m.shkut344@gmail.com>
2023-04-16 10:03:08 +0200

    3 exercise no enough

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     @State private var wakeUp = defaultWakeTime
-    @State private var coffeeAmount = 1.0
+    @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -21,13 +21,13 @@ struct ContentView: View {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
-
+            
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-
+            
             let sleepTime = wakeUp - prediction.actualSleep
             
             alertTitle = "Your ideal bedtime is…"
@@ -48,56 +48,61 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 0){
+            VStack {
+                Section(){
                     Text("When do you want to wake up?")
                         .font(.headline)
                     
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
-                VStack(alignment: .leading, spacing: 0){
+                Section(){
                     Text("Desired amount of sleep")
                         .font(.headline)
                     
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                Section(header: Text("Daily coffee intake").font(.largeTitle)){
+                    Picker("Coffee Amount", selection: $coffeeAmount) {
+                        ForEach(1..<20){
+                            Text($0, format: .number)
+                        }
+                    }
                 }
+                //Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                Text("Your best time is start sleeping \(alertMessage)")
             }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
+                .navigationTitle("BetterRest")
+                .toolbar {
+                    Button("Calculate", action: calculateBedtime)
+                }
+                .alert(alertTitle, isPresented: $showingAlert) {
+                    Button("OK") { }
+                } message: {
+                    Text(alertMessage)
+                }
+                
             }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
-
         }
     }
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
